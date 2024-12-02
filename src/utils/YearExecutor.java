05f4553f5eaf041acf7b45424fca09d1458eb790
sousa <sousanamain@gmail.com>
2024-12-02 09:14:28 +0000

    YearExecutor now executes all possible days

--- src/utils/YearExecutor.java
+++ src/utils/YearExecutor.java
@@ -2,7 +2,7 @@ package utils;
 
 public class YearExecutor {
     public static void main(String[] args) {
-        Year currentYear = new Year(2020);
+        Year currentYear = new Year(2024);
         currentYear.addAllPossibleDays();
         System.out.println(currentYear);
     }
