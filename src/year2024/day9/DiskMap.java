b00eb04c0dcfeaea6737635416fee07147f7456b
sousa <sousanamain@gmail.com>
2024-12-10 01:48:33 +0000

    i guess it looks somewhat better now? (1st part is a bit more complex to read but now takes use of the compressed memory blocks) maybe i just need to work on my naming conventions...

--- src/year2024/day9/DiskMap.java
+++ src/year2024/day9/DiskMap.java
@@ -1,207 +1,177 @@
 package year2024.day9;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-sealed interface MemoryBlock permits FilledBlock, FreeBlock {
-    int getID();
-    boolean isEmpty();
-}
-
-record FilledBlock(int id) implements MemoryBlock {
-    @Override
-    public int getID() {
-        return id;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-}
-
-record FreeBlock() implements MemoryBlock {
-    @Override
-    public int getID() {
-        return -1;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return true;
-    }
-}
 
 sealed interface CompressedMemoryBlock permits CompressedFilledBlock, CompressedEmptyBlock {
     int getID();
     boolean isEmpty();
     int getSize();
     long getChecksum();
+    int getStart();
 }
 
 record CompressedFilledBlock(int start, int id, int size) implements CompressedMemoryBlock {
     @Override
-    public int getID() {
-        return id;
-    }
-
+    public int getID() { return id; }
     @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
+    public boolean isEmpty() { return false; }
     @Override
-    public int getSize() {
-        return size;
-    }
-
+    public int getSize() { return size; }
     @Override
     public long getChecksum() {
         return IntStream.range(0, size)
-                .mapToLong(currIdx -> (long) (start + currIdx) * id).sum();
+                .mapToLong(currIdx -> (long) (start + currIdx) * id)
+                .sum();
     }
-
-    public int getStart() {
-        return start;
-    }
+    @Override
+    public int getStart() { return start; }
 }
 
 record CompressedEmptyBlock(int start, int size) implements CompressedMemoryBlock {
     @Override
-    public int getID() {
-        return -1;
-    }
-
+    public int getID() { return -1; }
     @Override
-    public boolean isEmpty() {
-        return true;
-    }
-
+    public boolean isEmpty() { return true; }
     @Override
-    public int getSize() {
-        return size;
-    }
-
+    public int getSize() { return size; }
     @Override
-    public long getChecksum() {
-        return 0L;
-    }
-
-    public int getStart() {
-        return start;
-    }
+    public long getChecksum() { return 0L; }
+    @Override
+    public int getStart() { return start; }
 }
 
 public class DiskMap {
-    private final List<MemoryBlock> memoryBlocks = new ArrayList<>();
     private final List<CompressedMemoryBlock> compressedMemoryBlocks = new ArrayList<>();
 
     public DiskMap(String input) {
-        int currentID = 0;
         int currentPosition = 0;
         for (int i = 0; i < input.length(); i++) {
-            int blockLength = Integer.parseInt(String.valueOf(input.charAt(i)));
+            int blockLength = Character.getNumericValue(input.charAt(i));
             if (i % 2 == 0) {
-                int filledBlockID = currentID;
-                IntStream.range(0, blockLength).forEach(idx -> memoryBlocks.add(new FilledBlock(filledBlockID)));
-                compressedMemoryBlocks.add(new CompressedFilledBlock(currentPosition, filledBlockID, blockLength));
-                currentID++;
+                compressedMemoryBlocks.add(new CompressedFilledBlock(currentPosition, i/2, blockLength));
             } else {
-                IntStream.range(0, blockLength).forEach(idx -> memoryBlocks.add(new FreeBlock()));
                 compressedMemoryBlocks.add(new CompressedEmptyBlock(currentPosition, blockLength));
             }
             currentPosition += blockLength;
         }
     }
 
-    public long getPart1() {
-        record IndexedMemoryBlock(int index, MemoryBlock block) {}
-        List<MemoryBlock> compactedFiles = getCompactedFiles();
-        return IntStream.range(0, compactedFiles.size())
-                .mapToObj(index -> new IndexedMemoryBlock(index, compactedFiles.get(index)))
-                .filter(indexedBlock -> !indexedBlock.block.isEmpty())
-                .mapToLong(indexedBlock -> (long) indexedBlock.index * indexedBlock.block.getID())
-                .sum();
-    }
-
-    private List<MemoryBlock> getCompactedFiles() {
-        List<MemoryBlock> blocks = new ArrayList<>(memoryBlocks);
-        for (int low = 0, high = blocks.size() - 1; low < high; low++, high--) {
-            low = findNextEmptyBlock(blocks, low);
-            high = findPreviousFilledBlock(blocks, high);
-
-            if (low >= high) break;
-
-            swapBlocks(blocks, low, high);
-        }
-        return blocks;
-    }
-
-    private int findNextEmptyBlock(List<MemoryBlock> blocks, int startIndex) {
-        while (startIndex <= blocks.size() && !blocks.get(startIndex).isEmpty()) {
-            startIndex++;
-        }
-        return startIndex;
-    }
-
-    private int findPreviousFilledBlock(List<MemoryBlock> blocks, int startIndex) {
-        while (startIndex >= 0 && blocks.get(startIndex).isEmpty()) {
-            startIndex--;
-        }
-        return startIndex;
-    }
-
-    private void swapBlocks(List<MemoryBlock> blocks, int emptyIndex, int filledIndex) {
-        blocks.set(emptyIndex, blocks.get(filledIndex));
-        blocks.set(filledIndex, new FreeBlock());
-    }
-
-    public long getPart2() {
-        List<CompressedFilledBlock> sortedFilledBlocks = compressedMemoryBlocks.stream()
+    private List<CompressedFilledBlock> getSortedCopyOfFilledBlocks() {
+        return compressedMemoryBlocks.stream()
                 .filter(block -> block instanceof CompressedFilledBlock)
                 .map(block -> (CompressedFilledBlock) block)
-                .sorted(Comparator.comparingInt(CompressedMemoryBlock::getID).reversed())
-                .toList();
+                .sorted(Comparator.comparingInt(CompressedFilledBlock::getStart).reversed())
+                .collect(Collectors.toList());
+    }
 
-        List<CompressedEmptyBlock> emptyBlocks = compressedMemoryBlocks.stream()
+    private List<CompressedEmptyBlock> getCopyOfEmptyBlocks() {
+        return compressedMemoryBlocks.stream()
                 .filter(block -> block instanceof CompressedEmptyBlock)
                 .map(block -> (CompressedEmptyBlock) block)
                 .collect(Collectors.toList());
+    }
 
-        return sortedFilledBlocks.stream()
-                .map(filledBlock -> reallocateFilledBlock(filledBlock, emptyBlocks))
+    public long getPart1() {
+        List<CompressedFilledBlock> remainingFilledBlocks = getSortedCopyOfFilledBlocks();
+        List<CompressedEmptyBlock> emptyBlocks = getCopyOfEmptyBlocks();
+
+        return getCompressedPart1(emptyBlocks, remainingFilledBlocks).stream()
                 .mapToLong(CompressedMemoryBlock::getChecksum)
                 .sum();
     }
 
-    private static CompressedFilledBlock reallocateFilledBlock(CompressedFilledBlock filledBlock, List<CompressedEmptyBlock> emptyBlocks) {
-        int filledBlockSize = filledBlock.getSize();
-        int filledBlockStart = filledBlock.getStart();
+    private static Deque<CompressedFilledBlock> getCompressedPart1(List<CompressedEmptyBlock> emptyBlocks, List<CompressedFilledBlock> remainingFilledBlocks) {
+        Deque<CompressedFilledBlock> result = new ArrayDeque<>();
 
-        for (CompressedEmptyBlock emptyBlock : emptyBlocks) {
-            int emptyBlockStart = emptyBlock.getStart();
-            int emptyBlockSize = emptyBlock.getSize();
-            if (emptyBlockStart >= filledBlockStart) {
+        while (!emptyBlocks.isEmpty() && !remainingFilledBlocks.isEmpty()) {
+            CompressedFilledBlock filledBlock = remainingFilledBlocks.getFirst();
+            CompressedEmptyBlock emptyBlock = emptyBlocks.getFirst();
+
+            if (filledBlock.getStart() < emptyBlock.getStart()) {
                 break;
             }
-            if (filledBlockSize <= emptyBlockSize) {
-                CompressedFilledBlock newFilledBlock = new CompressedFilledBlock(emptyBlockStart, filledBlock.getID(), filledBlockSize);
-                if (filledBlockSize == emptyBlockSize) {
-                    emptyBlocks.remove(emptyBlock);
-                } else {
-                    emptyBlocks.set(emptyBlocks.indexOf(emptyBlock), new CompressedEmptyBlock(emptyBlockStart + filledBlockSize, emptyBlockSize - filledBlockSize));
-                }
-                return newFilledBlock;
-            }
+
+            CompressedFilledBlock newFilledBlock = processBlock(filledBlock, emptyBlock,emptyBlocks,remainingFilledBlocks);
+            result.addLast(newFilledBlock);
         }
-        return filledBlock;
+
+        result.addAll(remainingFilledBlocks);
+        return result;
     }
 
-    @Override
-    public String toString() {
-        return memoryBlocks.toString();
+    private static CompressedFilledBlock processBlock(CompressedFilledBlock filledBlock,
+                                                      CompressedEmptyBlock emptyBlock,
+                                                      List<CompressedEmptyBlock> emptyBlocks,
+                                                      List<CompressedFilledBlock> remainingFilledBlocks) {
+        int filledBlockSize = filledBlock.getSize();
+        int emptyBlockSize = emptyBlock.getSize();
+        int emptyBlockStart = emptyBlock.getStart();
+
+        if (filledBlockSize > emptyBlockSize) {
+            CompressedFilledBlock newFilledBlock = new CompressedFilledBlock(emptyBlockStart, filledBlock.getID(), emptyBlockSize);
+            CompressedFilledBlock remainingFilledBlock = new CompressedFilledBlock(filledBlock.getStart(), filledBlock.getID(), filledBlockSize - emptyBlockSize);
+            emptyBlocks.removeFirst();
+            remainingFilledBlocks.set(0, remainingFilledBlock);
+            return newFilledBlock;
+        } else {
+            CompressedFilledBlock newFilledBlock = new CompressedFilledBlock(emptyBlockStart, filledBlock.getID(), filledBlockSize);
+            updateEmptyBlock(emptyBlocks, filledBlockSize, emptyBlockSize, 0, emptyBlockStart);
+            remainingFilledBlocks.removeFirst();
+            return newFilledBlock;
+        }
     }
+
+    public long getPart2() {
+        List<CompressedFilledBlock> remainingFilledBlocks = getSortedCopyOfFilledBlocks();
+        List<CompressedEmptyBlock> emptyBlocks = getCopyOfEmptyBlocks();
+
+        return getCompressedPart2(remainingFilledBlocks, emptyBlocks).stream()
+                .mapToLong(CompressedMemoryBlock::getChecksum)
+                .sum();
+    }
+
+    private static Deque<CompressedFilledBlock> getCompressedPart2(List<CompressedFilledBlock> remainingFilledBlocks, List<CompressedEmptyBlock> emptyBlocks) {
+        Deque<CompressedFilledBlock> result = new ArrayDeque<>();
+
+        for (CompressedFilledBlock filledBlock : remainingFilledBlocks) {
+            int filledBlockSize = filledBlock.getSize();
+            int filledBlockStart = filledBlock.getStart();
+            int i = 0;
+            for (; i < emptyBlocks.size(); i++) {
+                CompressedEmptyBlock emptyBlock = emptyBlocks.get(i);
+                if (emptyBlock.getStart() >= filledBlockStart) {
+                    break;
+                }
+                if (emptyBlock.getSize() >= filledBlockSize) {
+                    break;
+                }
+            }
+
+            if (i == emptyBlocks.size() || emptyBlocks.get(i).getStart() >= filledBlockStart || emptyBlocks.get(i).getSize() < filledBlockSize) {
+                result.addLast(filledBlock);
+                continue;
+            }
+
+            CompressedEmptyBlock emptyBlock = emptyBlocks.get(i);
+            int emptyBlockStart = emptyBlock.getStart();
+            int emptyBlockSize = emptyBlock.getSize();
+
+            CompressedFilledBlock newFilledBlock = new CompressedFilledBlock(emptyBlockStart, filledBlock.getID(), filledBlockSize);
+            updateEmptyBlock(emptyBlocks, filledBlockSize, emptyBlockSize, i, emptyBlockStart);
+            result.addLast(newFilledBlock);
+        }
+        return result;
+    }
+
+    private static void updateEmptyBlock(List<CompressedEmptyBlock> emptyBlocks, int filledBlockSize, int emptyBlockSize, int index, int emptyBlockStart) {
+        if (filledBlockSize == emptyBlockSize) {
+            emptyBlocks.remove(index);
+        } else {
+            CompressedEmptyBlock newEmptyBlock = new CompressedEmptyBlock(emptyBlockStart + filledBlockSize, emptyBlockSize - filledBlockSize);
+            emptyBlocks.set(index, newEmptyBlock);
+        }
+    }
+
 }
